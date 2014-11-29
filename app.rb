196426c76aa326184940bdaa94a2ccdf92b32577
Tejas Manohar <me@tejasmanohar.com>
2014-11-29 12:01:43 -0600

    ruby version so we're all on the same page

--- app.rb
+++ app.rb
@@ -2,11 +2,25 @@ require 'sinatra'
 require 'sinatra/flash'
 require 'sinatra/reloader' if development?
 
-require 'omniauth'
-require 'omniauth-twitter'
 require 'orchestrate'
 
 require 'pry' if development?
 
 require 'bundler'
 Bundler.require
+ 
+helpers do
+  def admin?
+    true
+  end
+end
+ 
+get '/public' do
+  "This is the public page - everybody is welcome!"
+end
+ 
+get '/private' do
+  halt(401,'Not Authorized') unless admin?
+  "This is the private page - members only"
+end
+
